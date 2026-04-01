a4138c5de2a32e5a9f8f230a36560091b5b60399
Rico Lai <157272599+u26jl23@users.noreply.github.com>
2026-04-01 22:28:15 +0800

    Add styling to preview posts

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,19 @@
 export default function Main() {
   return (
     <main>
-      <h1>Welcome to R1C0L's Blog!</h1>
-      <p className="welcome">
-        This is my personal corner on the internet. I want to use this site to post
-        thoughts, updates, and anything I find interesting.
-      </p>
-      <article>
-        <h2>Update</h2>
-        <small>April 1, 2026</small>
-        <p>
-          I am currently building this website step by step. Right now, I am working
-          on the homepage structure and learning how blog content should be organized.
+        <h1>Welcome to R1C0L's Blog!</h1>
+        <p className="welcome">
+          This is my personal corner on the internet. I want to use this site to post
+          thoughts, updates, and anything I find interesting.
         </p>
-      </article>
+        <article>
+          <h2>Update</h2>
+          <small>April 1, 2026</small>
+          <p>
+            I am currently building this website step by step. Right now, I am working
+            on the homepage structure and learning how blog content should be organized.
+          </p>
+        </article>
     </main>
   )
 }
