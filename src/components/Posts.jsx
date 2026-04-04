47b5d7101066607ff4bf6b50dca1aebc7b983514
Rico Lai <157272599+u26jl23@users.noreply.github.com>
2026-04-04 16:48:24 +0800

    Add some hardcoded posts for preview

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,6 +5,21 @@ export default function Posts(){
             <p>
                 This page will contain all blog posts.
             </p>
+            <article>
+                <h2>First Post</h2>
+                <small>April 4, 2026</small>
+                <p>
+                    This is the first sample post on the Posts page. Later, this page can show
+                    more complete post previews or even full post content.
+                </p>
+            </article>
+            <article>
+                <h2>Another Update</h2>
+                <small>April 4, 2026</small>
+                <p>
+                    This second post preview helps the Posts page look more like a real collection of entries.
+                </p>
+            </article>
         </main>
     )
 }
