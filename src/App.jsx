a31059b9826ca1c9ab4cdc1e0f19ea66ba52ae75
Rico Lai <157272599+u26jl23@users.noreply.github.com>
2026-04-04 16:41:03 +0800

    Add Posts page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar.jsx'
 import Main from './components/Main.jsx'
 import Footer from './components/Footer.jsx'
 import About from './components/About.jsx';
+import Posts  from './components/Posts.jsx';
 import '../style.css';
 
 // The top‑level component assembles the layout pieces
@@ -15,6 +16,7 @@ function App() {
       <Navbar />
       <Routes>
           <Route path='/' element={<Main/>} />
+          <Route path='/posts' element={<Posts/>}/>
           <Route path='/about' element={<About/>} />
       </Routes>
       <Footer />
---
+++ src/components/Posts.jsx
@@ -0,0 +1,10 @@
+export default function Posts(){
+    return (
+        <main>
+            <h1>Posts</h1>
+            <p>
+                This page will contain all blog posts.
+            </p>
+        </main>
+    )
+}
