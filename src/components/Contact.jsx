90117bbd5655b0d5ff4e78e3b66d4439017b2884
Rico Lai <157272599+u26jl23@users.noreply.github.com>
2026-04-14 21:29:33 +0800

    Create Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact(){
+    return(
+        <main>
+            <h1>Contact</h1>
+            <p>This page is where visitor can find ways to contact me.</p>
+        </main>
+    )
+}
