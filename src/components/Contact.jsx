83ed59c2ee6602765865878348eb2e0626795272
Rico Lai <157272599+u26jl23@users.noreply.github.com>
2026-04-14 21:35:46 +0800

    Update Contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,7 @@ export default function Contact(){
         <main>
             <h1>Contact</h1>
             <p>This page is where visitor can find ways to contact me.</p>
+            <p>contact methods will be added later</p>
         </main>
     )
 }
